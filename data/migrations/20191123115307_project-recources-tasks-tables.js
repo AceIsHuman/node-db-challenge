41e2d3884540071b8515566638a7204e5b0e08b1
AceIsHuman <menoman1995@yahoo.com>
2019-11-23 12:31:45 -0600

    create tasks table

--- data/migrations/20191123115307_project-recources-tasks-tables.js
+++ data/migrations/20191123115307_project-recources-tasks-tables.js
@@ -1,6 +1,26 @@
 
 exports.up = function(knex) {
-  
+  return (
+    knex.schema
+      .createTable('projects', tbl => {
+        tbl.increments();
+        tbl.text('name').notNullable();
+        tbl.string('description', 255);
+        tbl.boolean('completed');
+      })
+      .createTable('tasks', tbl => {
+        tbl.increments();
+        tbl.string('description', 255).notNullable();
+        tbl.string('notes', 255);
+        tbl.boolean('completed');
+        tbl.integer('project_id')
+          .unsigned()
+          .notNullable()
+          .references('projects.id')
+          .onDelete('CASCADE')
+          .onUpdate('CASCADE');
+      })
+  )
 };
 
 exports.down = function(knex) {
