1c1e8cbf3c575f3582daa561275aafa35ccfd299
AceIsHuman <menoman1995@yahoo.com>
2019-11-23 13:42:52 -0600

    add projectsRouter to server

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require('express');
 const helmet = require('helmet');
 
 const resourcesRouter = require('./resources/resourcesRoute');
+const projectsRouter = require('./projects/projectsRoute');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
 server.use('/api/resources/', resourcesRouter)
+server.use('/api/projects/', projectsRouter)
 
 module.exports = server;
