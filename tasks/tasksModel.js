37653e1cd0d5a341c3c50dfe3f724eae055cd649
AceIsHuman <menoman1995@yahoo.com>
2019-11-23 13:56:09 -0600

    join tables

--- tasks/tasksModel.js
+++ tasks/tasksModel.js
@@ -6,7 +6,9 @@ module.exports = {
 }
 
 function find() {
-  return db('tasks');
+  return db('tasks as t')
+    .join('projects as p', {'t.project_id' : 'p.id'})
+    .select('t.id as task_id', 't.description', 't.notes', 't.completed', 'p.name as project_name', 'p.description as project_description');
 }
 
 function insert(resource) {
