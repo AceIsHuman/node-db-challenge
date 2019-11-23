7a07d36b8b34cd340e8da2ad5bd769d79cb57440
AceIsHuman <menoman1995@yahoo.com>
2019-11-23 13:53:49 -0600

    add tasksRouter

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const helmet = require('helmet');
 
 const resourcesRouter = require('./resources/resourcesRoute');
 const projectsRouter = require('./projects/projectsRoute');
+const tasksRouter = require('./tasks/tasksRoute');
 
 const server = express();
 
 server.use(helmet());
 server.use(express.json());
-server.use('/api/resources/', resourcesRouter)
-server.use('/api/projects/', projectsRouter)
+server.use('/api/resources/', resourcesRouter);
+server.use('/api/projects/', projectsRouter);
+server.use('/api/tasks', tasksRouter);
 
 module.exports = server;
