91dfb84ff64ec793babdd5ef204629a2e8e0eea0
AceIsHuman <menoman1995@yahoo.com>
2019-11-23 13:32:01 -0600

    create endpoint to add

--- resources/resourcesRoute.js
+++ resources/resourcesRoute.js
@@ -8,4 +8,9 @@ router.get('/', async (req, res) => {
   res.status(200).json(resources);
 });
 
+router.post('/', async (req, res) => {
+  const [id] = await resources.insert(req.body);
+  res.status(201).json(id);
+});
+
 module.exports = router;
